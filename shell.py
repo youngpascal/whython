890a77c42e25b06bd82dc5aafd73b423f35554b5
youngpascal <wilburn.josha@gmail.com>
2020-03-01 20:01:50 -0600

    basic lexer and error handling

---
+++ shell.py
@@ -0,0 +1,12 @@
+import basic
+
+##################################
+# SHELL LOOP FOR INPUT
+##################################
+
+while True:
+    text = input('basic > ')
+    result, error = basic.run('<stdin>', text)
+
+    if error: print(error.as_string())
+    else: print(result)
